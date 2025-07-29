87808e76b504a4c0daeb3ebd1cf9d0ce208dd6fe
Sandesh <sandesh30apirl@gmail.com>
2025-07-29 23:30:07 +0530

    Prac

---
+++ 24_5.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+void Pattern(int iRow , int iCol)
+{
+   for(int i = 1 ; i<= iRow ; i++ )
+   {
+    for(int j = 1 ; j<= iCol ; j++)
+    {
+        if(i == j || i < j)
+        {
+            cout<<j<<"\t";
+        }
+        else
+        {
+            cout<<" \t";
+        }
+       
+    }
+    cout<<"\n";
+   }
+}
+
+int main()
+{
+    int iRow = 0 , iCol = 0;
+
+    cout<<"Enter the row and columns :"<<endl;
+    cin>>iRow>>iCol;
+
+    Pattern(iRow ,  iCol);
+
+    return 0;
+}
