dc9288599630eee7f98d048cc4b8a9fc84f69e06
Sandesh <sandesh30apirl@gmail.com>
2025-11-03 23:45:53 +0530

    prac

---
+++ Assi29_4.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Assi29_2
+{
+    public static void main(String a[])
+    {
+        String str = null ;
+        char cValue = '\0';
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the string :");
+        str = sobj.nextLine();
+
+        System.out.println("Enter the character to search : ");
+        cValue = sobj.next().charAt(0);
+
+        int  iRet = CharCount(str , cValue);
+
+            System.out.println(iRet);
+
+    }
+
+    public static int CharCount(String A , char ch)
+    {
+        int iPos = 0;
+        
+        for( int  i = 0 ; i < A.length() ; i++)
+        {
+            if(A.charAt(i) == ch)
+            {
+                iPos = i + 1;
+            }
+        }
+        return iPos;
+    }
+}
