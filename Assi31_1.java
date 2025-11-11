a412e40a93a9723085842bf870a910a41b81083d
Sandesh <sandesh30apirl@gmail.com>
2025-11-11 23:40:10 +0530

    Practice

---
+++ Assi31_1.java
@@ -0,0 +1,18 @@
+
+class Assi31_1
+{
+    public static void main(String A[])
+    {
+        String str = "Marvellous Python";
+
+
+
+        System.out.println("Hello world");
+    }
+}
+
+
+class Demo
+{
+    public void StrCpyRev()
+}
